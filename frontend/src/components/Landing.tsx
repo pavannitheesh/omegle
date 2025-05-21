94d8781ba20783249a2ebd15912594b6a1ab343a
pavannitheesh <gpavannitheesh1@gmail.com>
2025-05-21 22:53:12 +0530

    refactor: streamline camera handling in Landing component

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,26 +1,33 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom';
 
 export const Landing = () => {
-  const [localVideoStream,setLocalVideoStream] = useState< MediaStreamTrack | null>(null);
-  const [localAudioStream,setLocalAudioStream] = useState< MediaStreamTrack | null>(null);
-  const [name,setName] =useState("");
+  const [localStream, setLocalStream] = useState<MediaStream | null>(null);
+  const [name, setName] = useState("");
+  const videoRef = useRef<HTMLVideoElement | null>(null);
   const getCamera = async () => {
     const stream = await navigator.mediaDevices.getUserMedia({
       video: true,
       audio: true
     });
-    const video = stream.getVideoTracks()[0];
-    const audio = stream.getAudioTracks()[0];
-    setLocalVideoStream(video);
-    setLocalAudioStream(audio);
+    setLocalStream(stream);
+  };
 
-  }
-    useEffect(() => {
-      getCamera();
-    }, []);
+  useEffect(() => {
+    getCamera();
+  }, []);
+
+  useEffect(() => {
+    if (videoRef.current && localStream) {
+      videoRef.current.srcObject = localStream;
+      videoRef.current.onloadedmetadata = () => {
+        videoRef.current?.play();
+      };
+    }
+  }, [localStream]);
     return (
     <div>
+      <video ref={videoRef} height={400} width={400}></video>
         <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
         <Link to={`/room?name=${name}`} >Join</Link>
     </div>
